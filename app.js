065a87173a0b0db9646b61f784aae70850c8b936
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-12-27 18:26:20 -0500

    CORS enabled

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const bodyParser = require('body-parser');
 
@@ -31,6 +32,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
